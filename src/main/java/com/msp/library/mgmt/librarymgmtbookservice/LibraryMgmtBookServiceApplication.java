2f0200dbd927e4224b960795e0840f0ca8875460
Vasu Seelamneni <Vasu_Seelamneni@Dell.com>
2024-11-05 15:57:48 +0800

    Library management book services project

---
+++ src/main/java/com/msp/library/mgmt/librarymgmtbookservice/LibraryMgmtBookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.msp.library.mgmt.librarymgmtbookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMgmtBookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryMgmtBookServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msp/library/mgmt/librarymgmtbookservice/LibraryMgmtBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msp.library.mgmt.librarymgmtbookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMgmtBookServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
